2bc8e94ae02a7c05a45c0ca79f14f46cff3d17ca
Guizoul <guizoul.abdellah@ine.inpt.ma>
2023-02-07 13:12:01 +0100

    Initialized repository

---
+++ backend/src/main/java/com/videosharing/app/videosharingapp/VideoSharingAppApplication.java
@@ -0,0 +1,13 @@
+package com.videosharing.app.videosharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSharingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSharingAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/videosharing/app/videosharingapp/VideoSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videosharing.app.videosharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
