6e40dd02cc5e54a3a88f9ad9dffc4d32899b5124
Guizoul <guizoul.abdellah@ine.inpt.ma>
2023-02-08 11:56:33 +0100

    resolved pulling conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,5 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
-<<<<<<< HEAD
-import NavBar from "./Components/navbar/NavBar";
-import UploadVideo from "./Components/UploadVideo/uploadVideo";
-
-function App() {
-  return (
-    <>
-      <NavBar />
-      <UploadVideo />
-    </>
-=======
 import Home from "./pages/home/Home";
 import Navigation from "./pages/navigation/Navigation";
 
@@ -22,7 +11,6 @@ function App() {
         {/* <Route path="/about-us" element={<AboutUs />} /> */}
       </Route>
     </Routes>
->>>>>>> 571bde8765525911db68d4e801fdc82ae437828a
   );
 }
 
--- frontend/src/Components/navbar/NavBar.jsx
+++ frontend/src/Components/navbar/NavBar.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as UploadIcon } from "../../assets/upload.svg";
+import { ReactComponent as NotificationIcon } from "../../assets/notification.svg";
 
 const NavBar = () => {
   return (
@@ -12,8 +16,6 @@ const NavBar = () => {
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
-              <span className="sr-only">Open main menu</span>
-
               <svg
                 className="block h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +49,7 @@ const NavBar = () => {
             </button>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
+            <Link to={"/"} className="flex flex-shrink-0 items-center">
               <img
                 className="block h-9 w-auto lg:hidden"
                 src="./images/logo.png"
@@ -58,62 +60,53 @@ const NavBar = () => {
                 src="./images/logo.png"
                 alt="share it"
               />
-            </div>
+            </Link>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <a
-                  href="#a"
+                <Link
+                  to="/"
                   className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
                   aria-current="page"
                 >
-                  Dashboard
-                </a>
+                  Home
+                </Link>
 
-                <a
-                  href="#a"
+                <Link
+                  to="#a"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Team
-                </a>
+                </Link>
 
-                <a
-                  href="#a"
+                <Link
+                  to="#a"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Projects
-                </a>
+                  About us
+                </Link>
 
-                <a
-                  href="#a"
+                <Link
+                  to="#a"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Calendar
-                </a>
+                  More
+                </Link>
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-2">
+            <Link
+              to={"/upload-video"}
+              type="button"
+              className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <UploadIcon style={{ width: "24px", height: "24px" }} />
+            </Link>
             <button
               type="button"
               className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
             >
-              <span className="sr-only">View notifications</span>
-
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                />
-              </svg>
+              <NotificationIcon style={{ width: "24px", height: "24px" }} />
             </button>
 
             <div className="relative ml-3">
@@ -126,7 +119,7 @@ const NavBar = () => {
                   id="dropdownDefaultButton"
                   dataDropdownToggle="dropdown"
                 >
-                  <span className="sr-only">Open user menu</span>
+                  {" "}
                   <img
                     className="h-8 w-8 rounded-full"
                     src="./images/user.png"
